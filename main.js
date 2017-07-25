0f9e3e28724e896f0584d081de8e31e3549db7d2
erikr93 <erikr93@gmail.com>
2017-07-25 11:15:30 -0700

    created game logic

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-var cards = [" queen", " queen", " king", " king"]
+var cards = ["queen", "queen", "king", "king"];
+
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-flipCard(cardOne);
-
-var cardTwo = cards[1];
-
-
-var cardThree = cards[2];
-cardsInPlay.push(cardThree);
-flipCard(cardThree);
-
-var cardFour = cards[3];
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
-}  	else {
-		alert("Sorry, try again!") 
-}}
-
-function flipCard(i) {
-	console.log("user flipped" + i);
+		}
+ 	else {
+		alert("Sorry, try again!");
+		}
+	}
 }
+
+var flipCard = function(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+
